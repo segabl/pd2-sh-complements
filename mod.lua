00088163a416644dcc6bdfb97bc57db5fac4b936
Sebastian Gabl <sebastian.gabl@gmx.at>
2021-09-02 12:18:22 +0200

    Fix melee weapons ignoring weapon movement penalty

--- mod.lua
+++ mod.lua
@@ -27,7 +27,18 @@ if not StreamHeistComplements then
 
 end
 
-if RequiredScript == "lib/tweak_data/skilltreetweakdata" then
+if RequiredScript == "lib/tweak_data/blackmarkettweakdata" then
+
+	Hooks:PostHook(BlackMarketTweakData, "init", "shc_init", function (self)
+
+		-- Make melee weapons not ignore weapon movement penalty
+		for weap_id, weap_data in pairs(self.melee_weapons) do
+			weap_data.stats.remove_weapon_movement_penalty = nil
+		end
+
+	end)
+
+elseif RequiredScript == "lib/tweak_data/skilltreetweakdata" then
 
 	Hooks:PostHook(SkillTreeTweakData, "init", "shc_init", function (self)
 
