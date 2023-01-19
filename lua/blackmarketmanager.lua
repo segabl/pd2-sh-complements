2b249740bb05906dd87529435c30083dbf425765
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-01-20 00:19:06 +0100

    Fix bayonet decreasing weapon butt damage
    Instead of overriding the damage, bayonets will now add their damage and knockdown to the weapon butt base stats

--- lua/blackmarketmanager.lua
+++ lua/blackmarketmanager.lua
@@ -7,14 +7,14 @@ Hooks:OverrideFunction(BlackMarketManager, "equipped_melee_weapon_damage_info", 
 	local bayonet_id = self:equipped_bayonet(primary.weapon_id)
 	local player = managers.player:player_unit()
 
-	local bayonet
+	local dmg = math.lerp(stats.min_damage, stats.max_damage, lerp_value)
+	local dmg_effect = 0.1 * math.lerp(stats.min_damage_effect, stats.max_damage_effect, lerp_value)
+
 	if bayonet_id and player:movement():current_state()._equipped_unit:base():selection_index() == 2 and melee_entry == "weapon" then
 		stats = tweak_data.weapon.factory.parts[bayonet_id].stats
-		bayonet = true
+		dmg = dmg + math.lerp(stats.min_damage, stats.max_damage, lerp_value)
+		dmg_effect = dmg_effect + math.lerp(stats.min_damage_effect, stats.max_damage_effect, lerp_value)
 	end
-
-	local dmg = math.lerp(stats.min_damage, stats.max_damage, lerp_value)
-	local dmg_effect = (bayonet and dmg or 0.1) * math.lerp(stats.min_damage_effect, stats.max_damage_effect, lerp_value)
 
 	return dmg, dmg_effect
 end)
