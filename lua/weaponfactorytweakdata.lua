706de4b4b6e873d50456de07dd777708df1aa4f5
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-02-26 20:10:52 +0100

    Tweak conversion kit pickup rates

--- lua/weaponfactorytweakdata.lua
+++ lua/weaponfactorytweakdata.lua
@@ -9,4 +9,23 @@ Hooks:PostHook(WeaponFactoryTweakData, "create_ammunition", "shc_create_ammuniti
 	self.parts.wpn_fps_upg_a_slug.stats.moving_spread = 6
 	self.parts.wpn_fps_upg_a_slug.stats.spread = 6
 
+	-- Tweak pickup multipliers for conversion kits
+	self.parts.wpn_fps_ammo_type.custom_stats.ammo_pickup_max_mul = 0.5
+	self.parts.wpn_fps_ammo_type.custom_stats.ammo_pickup_min_mul = 0.5
+
+	self.parts.wpn_fps_lmg_hcar_body_conversionkit.custom_stats.ammo_pickup_max_mul = 2
+	self.parts.wpn_fps_lmg_hcar_body_conversionkit.custom_stats.ammo_pickup_min_mul = 2
+
+	self.parts.wpn_fps_ass_g3_b_short.custom_stats.ammo_pickup_max_mul = 2
+	self.parts.wpn_fps_ass_g3_b_short.custom_stats.ammo_pickup_min_mul = 2
+
+	self.parts.wpn_fps_pis_c96_b_long.custom_stats.ammo_pickup_max_mul = 0.5
+	self.parts.wpn_fps_pis_c96_b_long.custom_stats.ammo_pickup_min_mul = 0.5
+
+	self.parts.wpn_fps_upg_ass_ak_b_zastava.custom_stats.ammo_pickup_max_mul = 0.5
+	self.parts.wpn_fps_upg_ass_ak_b_zastava.custom_stats.ammo_pickup_min_mul = 0.5
+
+	self.parts.wpn_fps_ass_shak12_body_vks.custom_stats.ammo_pickup_max_mul = 0.5
+	self.parts.wpn_fps_ass_shak12_body_vks.custom_stats.ammo_pickup_min_mul = 0.5
+
 end)
