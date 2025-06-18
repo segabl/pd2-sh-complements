71547ea6cd389a4b6e035d33add2168d42a2fd02
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-06-18 21:28:33 +0200

    Slighty reduce font size of melee stats
    Fixes damage numbers not being fully visible

--- lua/blackmarketgui.lua
+++ lua/blackmarketgui.lua
@@ -1,9 +1,9 @@
--- Fix melee weapon knockdown stat display
-Hooks:PreHook(BlackMarketGui, "reload", "shc_reload", function (self)
+-- Fix melee weapon stat display
+Hooks:PreHook(BlackMarketGui, "reload", "shc_reload", function(self)
 	self._shc_patch = nil
 end)
 
-Hooks:PreHook(BlackMarketGui, "on_slot_selected", "shc_on_slot_selected", function (self)
+Hooks:PreHook(BlackMarketGui, "on_slot_selected", "shc_on_slot_selected", function(self)
 	if self._shc_patch then
 		return
 	end
@@ -12,6 +12,21 @@ Hooks:PreHook(BlackMarketGui, "on_slot_selected", "shc_on_slot_selected", functi
 			v.multiple_of = nil
 			self._shc_patch = true
 			return
+		end
+	end
+end)
+
+Hooks:PostHook(BlackMarketGui, "_setup", "_setup_pickup", function(self)
+	if not self._mweapon_stats_shown then
+		return
+	end
+
+	for _, stat in ipairs(self._mweapon_stats_shown) do
+		for column_name, text in pairs(self._mweapon_stats_texts[stat.name]) do
+			if column_name ~= "name" then
+				text:set_font_size(text:font_size() * 0.9)
+				text:set_vertical("center")
+			end
 		end
 	end
 end)
